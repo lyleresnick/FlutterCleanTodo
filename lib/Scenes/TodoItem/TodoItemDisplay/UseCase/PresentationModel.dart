2ce1856202e62e7e6c9e09f0618e093eef847262
Lyle Resnick <lyle@cellarpoint.com>
2023-09-11 00:37:46 -0400

    refactor

--- lib/Scenes/TodoItem/TodoItemDisplay/UseCase/PresentationModel.dart
+++ lib/Scenes/TodoItem/TodoItemDisplay/UseCase/PresentationModel.dart
@@ -4,32 +4,32 @@ part of '../TodoItemDisplay.dart';
 
 enum FieldName { title, note, completeBy, priority, completed }
 
-sealed class _RowPresentationModel {}
+sealed class _RowPresentationModel {
+  final FieldName field;
+  _RowPresentationModel(this.field);
+
+}
 
 @visibleForTesting
 class stringRow extends _RowPresentationModel {
-  final FieldName field;
   final String value;
-  stringRow(this.field, this.value);
+  stringRow(super.field, this.value);
 }
 
 @visibleForTesting
 class dateRow extends _RowPresentationModel {
-  final FieldName field;
   final DateTime value;
-  dateRow(this.field, this.value);
+  dateRow(super.field, this.value);
 }
 
 @visibleForTesting
 class boolRow extends _RowPresentationModel {
-  final FieldName field;
   final bool value;
-  boolRow(this.field, this.value);
+  boolRow(super.field, this.value);
 }
 
 @visibleForTesting
 class priorityRow extends _RowPresentationModel {
-  final FieldName field;
   final Priority value;
-  priorityRow(this.field, this.value);
+  priorityRow(super.field, this.value);
 }
